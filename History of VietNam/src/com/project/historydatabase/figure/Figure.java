1948304a522c873b9be2d087a164826687eed1b0
duyanh3101 <kazuu.promotion@gmail.com>
2023-07-10 10:18:06 +0700

    .

--- History of VietNam/src/com/project/historydatabase/figure/Figure.java
+++ History of VietNam/src/com/project/historydatabase/figure/Figure.java
@@ -4,6 +4,11 @@ public abstract class Figure {
     protected String namSinh;
     protected String namMat;
     protected String ten;
+    protected String tenKhac;
+    protected String queQuan;
+    protected String danToc;
+    protected String namNhapNgu;
+    protected String ghiChu;
     
     public Figure() {
     	
@@ -42,4 +47,48 @@ public abstract class Figure {
     public void setTen(String ten) {
         this.ten = ten;
     }
+    
+	public String getTenKhac() {
+		return tenKhac;
+	}
+	
+	public void setTenKhac(String tenKhac) {
+		this.tenKhac = tenKhac;
+	}
+	
+	public String getQueQuan() {
+		return queQuan;
+	}
+
+	public void setQueQuan(String queQuan) {
+		this.queQuan = queQuan;
+	}
+	
+	public String getDanToc() {
+		return danToc;
+	}
+
+	public void setDanToc(String danToc) {
+		this.danToc = danToc;
+	}
+	
+	public String namNhapNgu() {
+		return namNhapNgu;
+	}
+
+	public void setNamNhapNgu(String namNhapNgu) {
+		this.namNhapNgu = namNhapNgu;
+	}
+	
+	public String getGhiChu() {
+		return ghiChu;
+	}
+
+	public void setGhiChu(String ghiChu) {
+		this.ghiChu = ghiChu;
+	}
+	
+	
+
+	
 }
